d18b3c1e9026e9ac092117a125983f9dac72d545
Shinder Lin <shinder.lin@gmail.com>
2021-01-21 16:17:41 +0800

    a

---
+++ 程式碼/0121/6.setState或setXXX異步的解決方式/App.js
@@ -0,0 +1,13 @@
+import SelectBox from './components/SelectBox'
+import CountClass from './components/CountClass'
+
+function App() {
+  return (
+    <>
+      <SelectBox />
+      <CountClass />
+    </>
+  )
+}
+
+export default App
---
+++ 程式碼/0121/6.setState或setXXX異步的解決方式/components/CountClass.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+
+class CountClass extends Component {
+  constructor() {
+    super()
+    this.state = {
+      total: 0,
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <h1>{this.state.total}</h1>
+        <button
+          onClick={() => {
+            // 先計算最後的更新值，建立一個新的值的變數(or常數)
+            const newTotal = this.state.total + 1
+            // **後執行** 給react設定狀態用(呈現畫面用)
+            this.setState({ total: newTotal })
+            // **先執行** 作其它事用
+            console.log(newTotal)
+          }}
+        >
+          +1
+        </button>
+        <button
+          onClick={() => {
+            this.setState({ total: this.state.total - 1 })
+          }}
+        >
+          -1
+        </button>
+      </>
+    )
+  }
+}
+
+export default CountClass
