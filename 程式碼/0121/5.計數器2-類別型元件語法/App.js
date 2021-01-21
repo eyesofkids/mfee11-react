d1f16d5266a4a0d3370a9dc8d6e46067354fb520
Shinder Lin <shinder.lin@gmail.com>
2021-01-21 15:45:43 +0800

    add

---
+++ 程式碼/0121/5.計數器2-類別型元件語法/App.js
@@ -0,0 +1,15 @@
+import CountFunc from './components/CountFunc'
+import CountClass from './components/CountClass'
+
+function App() {
+  return (
+    <>
+      <h2>函式型元件</h2>
+      <CountFunc />
+      <h2>類別型元件</h2>
+      <CountClass />
+    </>
+  )
+}
+
+export default App
