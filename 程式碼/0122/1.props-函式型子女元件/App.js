0eac170a27047cab72c248549b6c33e468185937
Anna <anna@joomla.com.tw>
2021-01-22 09:24:29 +0800

    a

---
+++ 程式碼/0122/1.props-函式型子女元件/App.js
@@ -0,0 +1,11 @@
+import HelloWorld from './components/HelloWorld'
+
+function App() {
+  return (
+    <>
+      <HelloWorld text="你好" name="Amy" />
+    </>
+  )
+}
+
+export default App
---
+++ 程式碼/0122/1.props-函式型子女元件/components/HelloWorld.js
@@ -0,0 +1,9 @@
+function HelloWorld(props) {
+  return (
+    <h1>
+      {props.name}, {props.text}
+    </h1>
+  )
+}
+
+export default HelloWorld
