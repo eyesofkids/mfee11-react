9e22ad0c82b17a6ab3bef43aeebcf6ebd46050ec
Anna <anna@joomla.com.tw>
2021-01-22 11:32:45 +0800

    Update HelloWorldList.js

--- 程式碼/0122/5.jsx中列表map的用法/components/HelloWorldList.js
+++ 程式碼/0122/5.jsx中列表map的用法/components/HelloWorldList.js
@@ -2,11 +2,13 @@
 function HelloWorldList(props) {
   console.log(props)
   // 解構賦值
+  // names是陣列值
   const { names } = props
 
   return (
     <>
       <ul>
+        {/* 有用map記得加key值在最接近子元素中 */}
         {names.map((name, index) => {
           return <li key={index}>{name}，你好</li>
         })}
