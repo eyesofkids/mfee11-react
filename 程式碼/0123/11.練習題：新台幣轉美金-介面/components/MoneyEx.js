c7954cf8f1af62a1ebf32d66b0e534a1c069bf13
Anna <anna@joomla.com.tw>
2021-01-25 13:52:53 +0800

    ad

---
+++ 程式碼/0123/11.練習題：新台幣轉美金-介面/components/MoneyEx.js
@@ -0,0 +1,12 @@
+function MoneyEx() {
+  return (
+    <>
+      <input type="text" />
+      <button>新台幣轉美金</button>
+      <button>美金轉新台幣</button>
+      <h3>轉換過的金額是：</h3>
+    </>
+  )
+}
+
+export default MoneyEx
