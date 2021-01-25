60d72390e4bf9cd178d890b9aaee7cf29d873731
Anna <anna@joomla.com.tw>
2021-01-25 09:33:02 +0800

    u

--- 程式碼/0123/2.子女元件-父母元件-傳遞資料/components/Child.js
+++ 程式碼/0123/2.子女元件-父母元件-傳遞資料/components/Child.js
@@ -1,10 +1,10 @@
 function Child(props) {
-  console.log(props)
   const innerData = '子女元件內部資料'
 
   return (
     <button
       onClick={() => {
+        // 利用props得到的設定資料函式來設定資料給父母元件
         props.setData(innerData)
       }}
     >
