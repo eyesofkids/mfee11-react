028c23aabe37e62e578a16e851b251ba3e64f3af
Anna <anna@joomla.com.tw>
2021-01-27 10:28:23 +0800

    ad

---
+++ 程式碼/0127/4.練習題-待辨事項-1/components/TodoApp.js
@@ -0,0 +1,14 @@
+function TodoApp() {
+  return (
+    <>
+      <h1>待辨事項</h1>
+      <input type="text" />
+      <ul>
+        <li>買東西</li>
+        <li>買iphone</li>
+      </ul>
+    </>
+  )
+}
+
+export default TodoApp
