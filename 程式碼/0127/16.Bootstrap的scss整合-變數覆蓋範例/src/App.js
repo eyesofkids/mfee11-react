795409b098ec16ba618939f3c72698aab3dc4c49
Anna <anna@joomla.com.tw>
2021-01-27 16:24:14 +0800

    ad

---
+++ 程式碼/0127/16.Bootstrap的scss整合-變數覆蓋範例/src/App.js
@@ -0,0 +1,57 @@
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="alert alert-primary" role="alert">
+            A simple primary alert—check it out!
+          </div>
+          <div className="alert alert-secondary" role="alert">
+            A simple secondary alert—check it out!
+          </div>
+          <button type="button" className="btn btn-primary">
+            Primary
+          </button>
+          <button type="button" className="btn btn-secondary">
+            Secondary
+          </button>
+          <button type="button" className="btn btn-success">
+            Success
+          </button>
+          <button type="button" className="btn btn-danger">
+            Danger
+          </button>
+          <button type="button" className="btn btn-warning">
+            Warning
+          </button>
+          <button type="button" className="btn btn-info">
+            Info
+          </button>
+          <button type="button" className="btn btn-light">
+            Light
+          </button>
+          <button type="button" className="btn btn-dark">
+            Dark
+          </button>
+        </div>
+        <div className="row">
+          <nav aria-label="breadcrumb">
+            <ol className="breadcrumb">
+              <li className="breadcrumb-item">
+                <a href="#">Home</a>
+              </li>
+              <li className="breadcrumb-item">
+                <a href="#">Library</a>
+              </li>
+              <li className="breadcrumb-item active" aria-current="page">
+                Data
+              </li>
+            </ol>
+          </nav>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
