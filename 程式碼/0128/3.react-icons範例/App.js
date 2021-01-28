53f7bf89de3e37125ade54cac2ea7e070f9df77e
Anna <anna@joomla.com.tw>
2021-01-28 10:27:13 +0800

    Create App.js

---
+++ 程式碼/0128/3.react-icons範例/App.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react'
+
+import { FaReact } from 'react-icons/fa'
+import { Button } from 'react-bootstrap'
+
+function App() {
+  return (
+    <>
+      <Button variant="primary">
+        <FaReact /> React v16
+      </Button>
+    </>
+  )
+}
+
+export default App
