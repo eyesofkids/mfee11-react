155ce4d11a678cb9d862f4f008cac8669a5051d4
Anna <anna@joomla.com.tw>
2021-01-29 10:04:27 +0800

    Update App.js

--- 程式碼/0129/2.react-router整合b4樣版與Link元件使用範例/App.js
+++ 程式碼/0129/2.react-router整合b4樣版與Link元件使用範例/App.js
@@ -1,10 +1,13 @@
+// 使用套件
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 import React, { useState } from 'react'
 
+// 頁面用元件
 import Home from './pages/Home'
 import About from './pages/About'
 import Login from './pages/Login'
 
+// 組合用元件
 import MyNavbar from './components/MyNavbar'
 import MyFooter from './components/MyFooter'
 import MainContent from './components/MainContent'
@@ -15,7 +18,9 @@ function App() {
   return (
     <Router>
       <>
+        {/* LOGO+標題+導覽列+上方選單 */}
         <MyNavbar />
+        {/* 主內容區 */}
         <MainContent>
           <h2>用Link元件</h2>
           <Link to="/">Home</Link>
@@ -28,8 +33,10 @@ function App() {
           <a href="/about">About</a>
           <a href="/login">Login</a>
 
+          {/* 匹配路由表(路徑單一匹配) */}
           <Switch>
             <Route path="/login">
+              {/* 利用props傳入頁面元件狀態 */}
               <Login auth={auth} setAuth={setAuth} />
             </Route>
             <Route path="/about">
@@ -39,7 +46,9 @@ function App() {
               <Home auth={auth} />
             </Route>
           </Switch>
+          {/* end 匹配路由表 */}
         </MainContent>
+        {/* 頁尾+版權訊息 */}
         <MyFooter />
       </>
     </Router>
