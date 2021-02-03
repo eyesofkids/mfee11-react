1486dcb8f853e2ec2a1b5779f80fa0bcdff220d3
Anna <anna@joomla.com.tw>
2021-02-03 14:34:47 +0800

    ad

---
+++ 程式碼/0203/6.redux-thunk與伺服器相連-get/index.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.scss'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+
+// 第一步：匯入createStore
+import { createStore, compose, applyMiddleware } from 'redux'
+
+// 匯入串接的綁定器
+import { Provider } from 'react-redux'
+
+// 第二步：寫出reducer
+// 累加器/歸納器寫法
+import { rootReducer } from './reducers/index'
+
+// 導入redux-thunk中介軟體
+import thunk from 'redux-thunk'
+
+// 第三步：由rootReducer建立store
+// const store = createStore(
+//   rootReducer /* preloadedState, */,
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// )
+
+// 有中介軟體的語法
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(
+  rootReducer,
+  /* preloadedState, */ composeEnhancers(applyMiddleware(thunk))
+)
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
